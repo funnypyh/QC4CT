e838d8fac77b19d012fa1aec9214d31dc15466e8
Yeonhee Park <72807936+funnypyh@users.noreply.github.com>
2023-04-13 12:22:48 -0500

    Delete R directory

--- R/sample_kurtosis.R
+++
@@ -1,6 +0,0 @@
-sample_kurtosis <- function(x){
-  N = length(x)
-  kur = N*(N+1)/((N-1)*(N-2)*(N-3))*sum((x-mean(x))^4)/var(x)^2 - 3*(N-1)^2/((N-2)*(N-3))
-  
-  return(kur)
-}
--- R/sample_skewness.R
+++
@@ -1,6 +0,0 @@
-sample_skewness <- function(x){
-  N = length(x)
-  skew = N/(N-1)/(N-2)*sum((x-mean(x))^3)/sqrt(var(x)^3)
-  
-  return(skew)
-}
